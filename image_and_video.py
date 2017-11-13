b3a292e64661f2a19971b94071b26213f72dc1f0
zllrunning <zllrunning@gmail.com>
2017-11-13 20:46:59 +0800

    add file:video to image and image to video

---
+++ image_and_video.py
@@ -0,0 +1,28 @@
+#-*- coding:utf-8 -*-
+import cv2
+import imageio
+import os
+
+#video to image
+def video_to_image():
+    reader = imageio.get_reader('videoName.mp4')
+    i=0
+    for im in reader:
+        filename='imageVideo/'+str(i)+'.jpg'
+        i+=1
+        imageio.imwrite(filename,im)
+
+
+
+#image to Video
+def image_to_video():
+    img_root = 'imageVideo/'
+    fps = 24    #保存视频的FPS，可以适当调整
+    # print(len(os.listdir(img_root)))
+    fourcc = cv2.VideoWriter_fourcc(*'MJPG')
+    videoWriter = cv2.VideoWriter('saveVideo.mp4',fourcc,fps,(1920,1080))#最后一个是保存图片的尺寸
+     
+    for i in range(len(os.listdir(img_root))):
+        frame = cv2.imread(img_root+str(i)+'.jpg')
+        videoWriter.write(frame)
+    videoWriter.release()
